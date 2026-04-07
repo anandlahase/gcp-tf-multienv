55ca8d884c63a4cc5ec6235545a9c11097cbc3c6
anandlahase <lahase.anand@gmail.com>
2026-04-07 22:52:19 +0530

    vpn checkovficx

--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -8,4 +8,10 @@ resource "google_compute_subnetwork" "subnet" {
   ip_cidr_range = var.subnet_cidr
   region        = var.region
   network       = google_compute_network.vpc.id
+
+  log_config {
+    aggregation_interval = "INTERVAL_10_MIN"
+    flow_sampling        = 0.5
+    metadata             = "INCLUDE_ALL_METADATA"
+  }
 }
