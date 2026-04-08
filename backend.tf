278674259709b266695c383ef2a17c23398b22fe
anandlahase <lahase.anand@gmail.com>
2026-04-08 11:46:21 +0530

    update

--- backend.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket  = "cloudtech-tf-state-bucket"
-    prefix  = "landing-zone"
-  }
-}
---
+++ env/dev/backend.tf
@@ -0,0 +1,6 @@
+terraform {
+  backend "gcs" {
+    bucket  = "cloudtech-tf-state-dev"
+    prefix  = "dev"
+  }
+}
