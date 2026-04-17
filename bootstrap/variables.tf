2f607df530b996a452f9ce8be6831f05adf7c3f3
anandlahase <lahase.anand@gmail.com>
2026-04-17 13:11:10 +0530

    code update

--- bootstrap/variables.tf
+++ bootstrap/variables.tf
@@ -1,7 +1,7 @@
 variable "region" {}
 variable "billing_account" {}
 variable "bootstrap_project" {}
-
+variable "org_id" {}
 variable "dev_folder_id" {}
 variable "prod_folder_id" {}
 variable "shared_folder_id" {}
